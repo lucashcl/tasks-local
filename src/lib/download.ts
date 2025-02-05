16cdabf745e83a8c5f231904f724cc0e0b9ec0e7
Lucas Cipriano Lima <lucasc.lima26@gmail.com>
2025-02-05 16:27:48 -0300

    update download filename to include timestamp

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -2,8 +2,8 @@ export function download(blob: Blob) {
    const blobURL = URL.createObjectURL(blob)
    const link = document.createElement('a')
    link.href = blobURL
-   link.download = 'tasks.json'
+   link.download = `${new Date().toISOString()}_tasks.json`
    link.click()
    URL.revokeObjectURL(blobURL)
    link.remove()
-}
+}  
