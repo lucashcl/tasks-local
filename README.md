748a57ed6ca7d8de4d66d0e540b2daedf012aa6e
Lucas Cipriano Lima <lucasc.lima26@gmail.com>
2025-07-04 11:44:33 -0300

    Adds helper text when no tasks exist

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ This is a simple tasks app built with react, shadcn and a few more libs
 - Fix some issues and update the project
 
 ### Todo
-[X] Add horizontal padding\
-[ ] Show helper when there's no tasks added yet\
+[X] Show helper when there's no tasks added yet\
 [ ] Show routine of task\
 [ ] Random task picker modal\
 [ ] Sync tasks
--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,31 @@ function App() {
     <main className="flex flex-col min-h-screen relative">
       <TaskPallete />
       <Header progress={progress} />
-      <section className='flex flex-col flex-1 gap-4 p-4 h-full mx-80'>
-        {currentTasks.map(task => (
-          <TaskCard
-            key={task.id}
-            {...task}
-            onToggle={() => toggleTask(task.id)}
-            onRemove={() => removeTask(task.id)}
-          />
-        ))}
-      </section>
+        {
+            currentTasks.length > 0 ?
+                <section className='flex flex-col flex-1 gap-4 p-4 h-full mx-80'>
+                    {currentTasks.map(task => (
+                        <TaskCard
+                            key={task.id}
+                            {...task}
+                            onToggle={() => toggleTask(task.id)}
+                            onRemove={() => removeTask(task.id)}
+                        />
+                    ))}
+                </section>
+                :
+                <div className="w-full h-full flex items-center justify-center my-auto">
+                    <p className="text-muted-foreground text-sm text-center ">
+                        Press{" "}
+                        <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+                            <span className="text-xs">Ctrl + </span>K
+                        </kbd>
+                        {" "}to
+                        <br/>
+                        add a new task
+                    </p>
+                </div>
+        }
       <div className='lg:hidden'>
         <Separator />
         <div className='flex gap-2 p-2'>
