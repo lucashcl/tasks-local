cf0b98525c60b8f08b4b2a1cb23f1ed75b563fbd
Lucas Cipriano Lima <lucasc.lima26@gmail.com>
2025-07-04 10:37:11 -0300

    Update project dependencies

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 This is a simple tasks app built with react, shadcn and a few more libs
 
-### 06-02-2025
+### 04-07-2025
 - Created a complete create task component
 
 ### Todo
-- [X] Add context menu for deletion
-- [X] Create a settings button
-- Sync tasks
+[X] Update dependencies\
+[ ] Sort tasks\
+[ ] Add horizontal padding\
+[ ] Show helper when there's no tasks added yet\
+[ ] Show routine of task\
+[ ] Random task picker modal\
+[ ] Sync tasks
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tasks-local/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
